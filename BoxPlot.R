b345f1dba88c3593236b8139ae2bfa9eab102bc7
Alexander Reinthal <alexander.reinthal@gmail.com>
2017-04-18 13:24:36 +0200

    minor changes

--- BoxPlot.R
+++ BoxPlot.R
@@ -33,89 +33,89 @@ data(Computers,package = "Ecdat")
 ########## Start with BudgetUK
 
 # Convert numerical to class
-newfac <- rep(0,dim(BudgetUK)[1])
-newfac[BudgetUK$children == 1] <-"One"
-newfac[BudgetUK$children == 2] <-"Two"
-newfac <- as.factor(newfac)
-BudgetUK$children <- newfac
+newfac  <-  rep(0,dim(BudgetUK)[1])
+newfac[BudgetUK$children == 1]  <- "One"
+newfac[BudgetUK$children == 2]  <- "Two"
+newfac  <-  as.factor(newfac)
+BudgetUK$children  <-  newfac
 
 
 # create data partitions for training and testing
-inTrain <- createDataPartition( BudgetUK$children , p=3/4, list=FALSE)
+inTrain  <-  createDataPartition( BudgetUK$children , p=3/4, list=FALSE)
 
 # Training data
-trainW <- BudgetUK[inTrain,-10]
-testW <- BudgetUK[-inTrain,-10]
+trainW  <-  BudgetUK[inTrain,-10]
+testW  <-  BudgetUK[-inTrain,-10]
 
 # Training labels
-trainwY <- BudgetUK$children[inTrain]
-testwY <- BudgetUK$children[-inTrain]
+trainwY  <-  BudgetUK$children[inTrain]
+testwY  <-  BudgetUK$children[-inTrain]
 
 # Compute the error B times
-B<-50
-ERRMAT<-matrix(0,B,8)
+B  <-  50
+ERRMAT  <-  matrix(0,B,8)
 
 # set the validation method to cross validation
-ctrl<-trainControl(method="cv",summaryFunction=multiClassSummary)
+ctrl  <-  trainControl(method="cv",summaryFunction=multiClassSummary)
 
 
 
 # Loop takes 1 hour for BudgetUK
 for (b in (1:B)) {
   
-  inTrain <- createDataPartition(BudgetUK$children,p=3/4,list=FALSE)
-  trainW <- BudgetUK[inTrain,-10]
-  testW <- BudgetUK[-inTrain,-10]
-  trainwY <- BudgetUK$children[inTrain]
-  testwY <- BudgetUK$children[-inTrain]
+  inTrain  <-  createDataPartition(BudgetUK$children,p=3/4,list=FALSE)
+  trainW  <-  BudgetUK[inTrain,-10]
+  testW  <-  BudgetUK[-inTrain,-10]
+  trainwY  <-  BudgetUK$children[inTrain]
+  testwY  <-  BudgetUK$children[-inTrain]
   
   
   # CART
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="rpart",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="rpart",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
   
   # Random Forest
-  ERRMAT[b,1]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  ERRMAT[b,1] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
   fit<-train(children~.,data=BudgetUK[inTrain,],method="ranger",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw") 
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw") 
   
   
   # KNN
-  ERRMAT[b,2]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="knn",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  ERRMAT[b,2] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="knn",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
   
   # LDA
-  ERRMAT[b,3]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="lda",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgeyestUK[-inTrain,-10],type="raw")
+  ERRMAT[b,3] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="lda",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgeyestUK[-inTrain,-10],type="raw")
   
   
   # QDA
-  ERRMAT[b,4]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="qda",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  ERRMAT[b,4] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="qda",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
   # PDA
-  ERRMAT[b,5]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="pda",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  ERRMAT[b,5] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="pda",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
   
   # NB - Negative Binomial
-  ERRMAT[b,6]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="nb",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  ERRMAT[b,6] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="nb",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
   
   # MDA - mixture da
-  ERRMAT[b,7]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="mda",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  ERRMAT[b,7] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="mda",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
-  ERRMAT[b,8]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  ERRMAT[b,8] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
   print(b)
 }
 
@@ -126,99 +126,101 @@ bp <- boxplot(ERRMAT,
               col = 2:8,
               names=c("CART","RF","knn","lda","qda","pda","nb","mda"))
 
-######### B??rja skriva kod h??r
+######### Boerja skriva kod haer
 ########## Continue with Computers
 
+
+
 # Convert numerical to class
-newfac <- rep(0,dim(BudgetUK)[1])
-newfac[BudgetUK$children == 1] <-"One"
-newfac[BudgetUK$children == 2] <-"Two"
+newfac  <-  rep(0,dim(BudgetUK)[1])
+newfac[BudgetUK$children == 1]  <- "One"
+newfac[BudgetUK$children == 2]  <- "Two"
 newfac <- as.factor(newfac)
-BudgetUK$children <- newfac
+BudgetUK$children  <-  newfac
 
 
 # create data partitions for training and testing
-inTrain <- createDataPartition( BudgetUK$children , p=3/4, list=FALSE)
+inTrain  <-  createDataPartition( BudgetUK$children , p=3/4, list=FALSE)
 
 # Training data
-trainW <- BudgetUK[inTrain,-10]
-testW <- BudgetUK[-inTrain,-10]
+trainW  <-  BudgetUK[inTrain,-10]
+testW  <-  BudgetUK[-inTrain,-10]
 
 # Training labels
-trainwY <- BudgetUK$children[inTrain]
-testwY <- BudgetUK$children[-inTrain]
+trainwY  <-  BudgetUK$children[inTrain]
+testwY  <-  BudgetUK$children[-inTrain]
 
 # Compute the error B times
-B<-50
-ERRMAT<-matrix(0,B,8)
+B <- 50
+ERRMAT <- matrix(0,B,8)
 
 # set the validation method to cross validation
-ctrl<-trainControl(method="cv",summaryFunction=multiClassSummary)
+ctrl <- trainControl(method="cv",summaryFunction=multiClassSummary)
 
 
 
 # Loop takes 1 hour for BudgetUK
 for (b in (1:B)) {
   
-  inTrain <- createDataPartition(BudgetUK$children,p=3/4,list=FALSE)
-  trainW <- BudgetUK[inTrain,-10]
-  testW <- BudgetUK[-inTrain,-10]
-  trainwY <- BudgetUK$children[inTrain]
-  testwY <- BudgetUK$children[-inTrain]
+  inTrain  <-  createDataPartition(BudgetUK$children,p=3/4,list=FALSE)
+  trainW  <-  BudgetUK[inTrain,-10]
+  testW  <-  BudgetUK[-inTrain,-10]
+  trainwY  <-  BudgetUK$children[inTrain]
+  testwY  <-  BudgetUK$children[-inTrain]
   
   
   # CART
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="rpart",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="rpart",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
   
   # Random Forest
-  ERRMAT[b,1]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="ranger",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw") 
+  ERRMAT[b,1] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="ranger",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw") 
   
   
   # KNN
-  ERRMAT[b,2]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="knn",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  ERRMAT[b,2] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="knn",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
   
   # LDA
-  ERRMAT[b,3]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="lda",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgeyestUK[-inTrain,-10],type="raw")
+  ERRMAT[b,3] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="lda",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgeyestUK[-inTrain,-10],type="raw")
   
   
   # QDA
-  ERRMAT[b,4]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="qda",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  ERRMAT[b,4] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="qda",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
   # PDA
-  ERRMAT[b,5]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="pda",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  ERRMAT[b,5] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="pda",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
   
   # NB - Negative Binomial
-  ERRMAT[b,6]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="nb",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  ERRMAT[b,6] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="nb",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
   
   # MDA - mixture da
-  ERRMAT[b,7]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
-  fit<-train(children~.,data=BudgetUK[inTrain,],method="mda",tuneLength=15,trControl=ctrl)
-  pp<-predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
+  ERRMAT[b,7] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  fit <- train(children~.,data=BudgetUK[inTrain,],method="mda",tuneLength=15,trControl=ctrl)
+  pp <- predict(fit,newdata=BudgetUK[-inTrain,-10],type="raw")
   
-  ERRMAT[b,8]<-length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
+  ERRMAT[b,8] <- length(pp[pp!=BudgetUK$children[-inTrain]])/length(pp)
   print(b)
 }
 
 
 
-bp <- boxplot(ERRMAT,
+bp  <-  boxplot(ERRMAT,
               ylab="Cross Validation Error",
               col = 2:8,
               names=c("CART","RF","knn","lda","qda","pda","nb","mda"))
--- Ensemble.R
+++ Ensemble.R
@@ -1,7 +1,23 @@
-ensamble <- function(data, method, nrClassifiers){
+ensemble <- function(labels, data, method, nrEnsembles){
+  
   # split the data
+  indeces <- createFolds(labels, k = nrEnsembles)
+  
   
   # train the classifier on each of the splits
+  for (fold in indeces){
+    
+    # Training data
+    trainX <- data[fold]
+    testX <- data[-fold]
+    
+    # Training labels
+    trainY <- labels[fold]
+    testY <- labels[-fold]
+    
+    
+  }
+  
   
   # count the votes
   
