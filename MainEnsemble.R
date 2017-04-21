1ac4bef31b00aeb3f93a8d89337dacde21044797
Alexander Reinthal <alexander.reinthal@gmail.com>
2017-04-21 09:41:16 +0200

    cleaned up code agani

--- MainEnsemble.R
+++ MainEnsemble.R
@@ -70,62 +70,33 @@ err <- ensemble(my_formula2, predict_col2,curr_data2,curr_method2,nr_classifiers
 ## Main for plots
 
 # Compute the error B times
-B <- 50
-ERRMAT1 <- matrix(0,B,6)
-ERRMAT2 <- matrix(0,B,6)
+B <- 1
+
+methods <- c("rpart","knn","lda","qda","nb")
+ERRMAT1 <- matrix(0,B,length(methods))
+ERRMAT2 <- matrix(0,B,length(methods))
 
 for (b in (1:B)) {
-  print("iteration: ")
-  print(b)
-  
-  # CART
-  ERRMAT1[b,1] <- ensemble(my_formula1, predict_col1, curr_data1, "rpart", nr_classifiers1)
-  ERRMAT2[b,1] <- ensemble(my_formula2, predict_col2,curr_data2,"rpart",nr_classifiers2)
-  
-  # KNN 
-  
-  ERRMAT1[b,2] <- ensemble(my_formula1, predict_col1, curr_data1, "knn", nr_classifiers1)
-  ERRMAT2[b,2] <- ensemble(my_formula2, predict_col2,curr_data2,"knn",nr_classifiers2)
-  
-  # LDA 
-
-  ERRMAT1[b,3] <- ensemble(my_formula1, predict_col1, curr_data1, "lda", nr_classifiers1)
-  ERRMAT2[b,3] <- ensemble(my_formula2, predict_col2,curr_data2,"lda",nr_classifiers2)
-  
-  # QDA
-  
-  ERRMAT1[b,4] <- ensemble(my_formula1, predict_col1, curr_data1, "qda", nr_classifiers1)
-  ERRMAT2[b,4] <- ensemble(my_formula2, predict_col2,curr_data2,"qda",nr_classifiers2)
-  
-  # PDA
-  
-  ERRMAT1[b,5] <- ensemble(my_formula1, predict_col1, curr_data1, "pda", nr_classifiers1)
-  ERRMAT2[b,5] <- ensemble(my_formula2, predict_col2,curr_data2,"pda",nr_classifiers2)
-  
-  # NB - Naive bayes 
-  
-  ERRMAT1[b,6] <- ensemble(my_formula1, predict_col1, curr_data1, "nb", nr_classifiers1)
-  ERRMAT2[b,6] <- ensemble(my_formula2, predict_col2,curr_data2,"nb",nr_classifiers2)
-  
+  print( paste("iteration: ", b) )
+  print("-----------")
+  for (method in methods) {
+    print('method: ')
+    print(method)
+    ERRMAT1[b,1] <- ensemble(my_formula1, predict_col1, curr_data1, method, nr_classifiers1)
+    ERRMAT2[b,1] <- ensemble(my_formula2, predict_col2, curr_data2, method, nr_classifiers2)
+  }  
 }
 
-# jpeg('/path/to/project/mve440_mini2/figures/BudgetUK-ENSEMBLE.png')
-jpeg('/chalmers/users/reinthal/Master/repos/mve440_mini2/figures/BudgetUK-ENSEMBLE.jpeg')
 
 bp  <-  boxplot(ERRMAT1,
                 ylab="Validation Error",
-                col = 2:7,
-                names=c("CART","knn","lda","qda","pda","nb"),
+                col = 2:(length(methods)-1),
+                names=methods,
                 main=c("BudgetUK"))
-dev.off()
 
 
-# Same here - add the relevant path
-jpeg('/chalmers/users/reinthal/Master/repos/mve440_mini2/figures/Computers-ENSEMBLE.jpg')
 bp  <-  boxplot(ERRMAT2,
                 ylab="Validation Error ",
-                col = 2:7,
-                names=c("CART","knn","lda","qda","pda","nb"), 
+                col = 2:(length(methods)-1),
+                names=methods, 
                 main=c("Computers"))
-
- def.off()
